9dc9b7ab864e1a589f2306620ecb9f53f8531fcd
Samuel Smith <92334211+SmithSamP@users.noreply.github.com>
2022-05-25 11:03:24 -0700

    Started integration

--- slider.py
+++ slider.py
@@ -1,12 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
-
+from scipy.integrate import quad
 
 # The parametrized function to be plotted
+u = np.linspace(0, 2*np.pi, 100)
+epsilon = 0.2
+A = np.sqrt((1-epsilon)/(1+epsilon))
+h = 3
+P = 1
 
 def fx(u, A, epsilon, h):
-    return h*(np.cos(u)+epsilon)
+    return (h*(np.cos(u)+epsilon))/(1+epsilon)
 
 def fy(u, A, epsilon, h):
     return h*A*np.sin(u)
@@ -14,11 +19,12 @@ def fy(u, A, epsilon, h):
 def fA(eps):
     return np.sqrt((1-eps)/(1+eps))
 
-u = np.linspace(0, 2*np.pi, 100)
-epsilon = 0.2
-A = np.sqrt((1-epsilon)/(1+epsilon))
+def fq1Deriv(u, A, epsilon, h, P):
+    return ((((epsilon*np.sin(u)+(1+epsilon))*np.sqrt(P**2*(1-epsilon*np.cos(u)/(1-epsilon*np.cos(u)))-A**2)))/(1-epsilon*np.cos(u)))/h
 
-h = 9
+def fq1Integrated(u, A, epsilon, h, P):
+
+    xpos = (h*(np.cos(u)+epsilon))/(1+epsilon)
 
 # Create the figure and the line that we will manipulate
 fig, ax = plt.subplots()
@@ -26,8 +32,9 @@ fig, ax = plt.subplots()
 fig.set_figwidth(7)
 fig.set_figheight(10)
 
-line, = plt.plot(fx(u,epsilon,A,h), fy(u,epsilon,A,h), lw=2)
-ax.set_xlabel('Time [s]')
+line, = plt.plot(fx(u,A,epsilon,h), fy(u,A,epsilon,h), lw=2)
+q1, = plt.plot(3,3, 'ro')
+
 plt.axhline(y = 0, color="black", linestyle="--")
 plt.axvline(x = 0, color="black", linestyle="--")
 plt.xlim([-10, 10])
@@ -40,8 +47,8 @@ plt.subplots_adjust(left=0.25, bottom=0.25)
 axh = plt.axes([0.25, 0.1, 0.65, 0.03])
 h_slider = Slider(
     ax=axh,
-    label='Frequency [Hz]',
-    valmin=-10,
+    label='Function of Time',
+    valmin=0,
     valmax=10,
     valinit=h,
 )
@@ -50,7 +57,7 @@ h_slider = Slider(
 axepsilon = plt.axes([0.1, 0.25, 0.0225, 0.63])
 epsilon_slider = Slider(
     ax=axepsilon,
-    label="Amplitude",
+    label="Ecentricity (Wind)",
     valmin=0,
     valmax=1,
     valinit=epsilon,
@@ -64,8 +71,8 @@ epsilon_slider = Slider(
 #     fig.canvas.draw_idle()
 
 def update(val):
-    line.set_xdata(fx(u, epsilon_slider.val, fA(epsilon_slider.val), h_slider.val))
-    line.set_ydata(fy(u, epsilon_slider.val, fA(epsilon_slider.val), h_slider.val))
+    line.set_xdata(fx(u, fA(epsilon_slider.val), epsilon_slider.val,  h_slider.val))
+    line.set_ydata(fy(u, fA(epsilon_slider.val), epsilon_slider.val,  h_slider.val))
     fig.canvas.draw_idle()
 
 
